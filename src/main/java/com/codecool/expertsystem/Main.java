289af6e42e868e3eb44452be5c5ec02d8f90b4a1
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 12:59:46 +0200

    Implament basic Main class

--- src/main/java/com/codecool/expertsystem/Main.java
+++ src/main/java/com/codecool/expertsystem/Main.java
@@ -1,1 +1,18 @@
 package com.codecool.expertsystem;
+
+import com.codecool.expertsystem.controller.ESProvider;
+import com.codecool.expertsystem.model.parsers.FactParser;
+import com.codecool.expertsystem.model.parsers.RuleParser;
+import com.codecool.expertsystem.view.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        View view = new View();
+        ESProvider esProvider = new ESProvider(FactParser, RuleParser);
+
+        
+        
+    }
+
+}
