24904a8e5100c03218d384583d7bfa54220445d9
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 16:07:39 +0200

    Implement Question class Extend addRuleToRepository method in RuleParser class

--- src/main/java/com/codecool/expertsystem/model/containers/Question.java
+++ src/main/java/com/codecool/expertsystem/model/containers/Question.java
@@ -1,24 +1,30 @@
 package com.codecool.expertsystem.model.containers;
 
 public class Question {
+    private String id;
+    private String question;
+    private Answer answer;
 
-    public Question(String Id, String question, Answer answer) {
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
 
     }
 
     public String getId() {
-
+        return id;
     }
 
     public String getQuestion() {
-
+        return question;
     }
 
     public Answer getAnswer() {
-
+        return answer;
     }
 
     public boolean getEvaluatedAnswer(String input) {
-
+        return this.answer.evaluateAnswerByInput(input);
     }
 }
--- src/main/java/com/codecool/expertsystem/model/parsers/RuleParser.java
+++ src/main/java/com/codecool/expertsystem/model/parsers/RuleParser.java
@@ -1,7 +1,9 @@
 package com.codecool.expertsystem.model.parsers;
 
 import com.codecool.expertsystem.model.containers.RuleRepository;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 public class RuleParser extends XMLParser {
     private RuleRepository ruleRepository;
@@ -9,6 +11,7 @@ public class RuleParser extends XMLParser {
 
     public RuleParser(String xmlPath) {
         this.ruleRepository = new RuleRepository();
+        this.nodeList = document.getElementsByTagName("Rule");
         loadXmlDocument(xmlPath);
         addRulesToRepository();        
     }
@@ -18,7 +21,52 @@ public class RuleParser extends XMLParser {
     }
 
     private addRulesToRepository() {
-        this.nodeList = document.getElementsByTagName("Rule");
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node tempNode = nodeList.item(count);
+
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) tempNode;
+
+                eElement.getAttribute("id"); // id pytania np dog
+                eElement.getElementsByTagName("Question").item(0).getTextContent(); // tekstowa reprezentacja pytania
+
+        
+                // get node name and value
+                System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+                System.out.println("Node Value =" + tempNode.getTextContent());
+        
+                if (tempNode.hasAttributes()) {
+        
+                    // get attributes names and values
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+        
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+        
+                        Node node = nodeMap.item(i);
+                        System.out.println("attr name : " + node.getNodeName());
+                        System.out.println("attr value : " + node.getNodeValue());
+        
+                    }
+        
+                }
+        
+                if (tempNode.hasChildNodes()) {
+        
+                    // loop again if has child nodes
+                    printNote(tempNode.getChildNodes());
+        
+                }
+        
+                System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+        
+            }
+        
+            }
+        
+          }
+        
+        
         if (document.hasChildNodes()) {
             document.getChildNodes();
         }
