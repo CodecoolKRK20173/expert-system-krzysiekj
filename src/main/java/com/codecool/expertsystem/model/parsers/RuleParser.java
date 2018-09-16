90c499830c0b627806b64523e9d0ba888f1c1f6f
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 18:16:09 +0200

    Implement addRulesToRepository and getAnswers methods in RuleParser class

--- src/main/java/com/codecool/expertsystem/model/parsers/RuleParser.java
+++ src/main/java/com/codecool/expertsystem/model/parsers/RuleParser.java
@@ -1,6 +1,14 @@
 package com.codecool.expertsystem.model.parsers;
 
+import com.codecool.expertsystem.model.containers.Answer;
 import com.codecool.expertsystem.model.containers.RuleRepository;
+import com.codecool.expertsystem.model.containers.Question;
+import com.codecool.expertsystem.model.containers.MultipleValue;
+import com.codecool.expertsystem.model.containers.SingleValue;
+import com.codecool.expertsystem.model.containers.Value;
+
+import java.util.Arrays;
+import java.util.List;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -13,63 +21,57 @@ public class RuleParser extends XMLParser {
         this.ruleRepository = new RuleRepository();
         this.nodeList = document.getElementsByTagName("Rule");
         loadXmlDocument(xmlPath);
-        addRulesToRepository();        
+        addRulesToRepository();
     }
 
-    public RuleRepository getRulesRepository() {        
+    public RuleRepository getRuleRepository() {
         return this.ruleRepository;
     }
 
-    private addRulesToRepository() {
+    private void addRulesToRepository() {
 
         for (int count = 0; count < nodeList.getLength(); count++) {
             Node tempNode = nodeList.item(count);
 
             if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) tempNode;
+                Element tempElement = (Element) tempNode;
 
-                eElement.getAttribute("id"); // id pytania np dog
-                eElement.getElementsByTagName("Question").item(0).getTextContent(); // tekstowa reprezentacja pytania
+                String id = tempElement.getAttribute("id");
+                String questionContent = tempElement.getElementsByTagName("Question")
+                                                    .item(0).getTextContent();
+                Answer answer = getAnswers(tempElement);
 
-        
-                // get node name and value
-                System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
-                System.out.println("Node Value =" + tempNode.getTextContent());
-        
-                if (tempNode.hasAttributes()) {
-        
-                    // get attributes names and values
-                    NamedNodeMap nodeMap = tempNode.getAttributes();
-        
-                    for (int i = 0; i < nodeMap.getLength(); i++) {
-        
-                        Node node = nodeMap.item(i);
-                        System.out.println("attr name : " + node.getNodeName());
-                        System.out.println("attr value : " + node.getNodeValue());
-        
-                    }
-        
-                }
-        
-                if (tempNode.hasChildNodes()) {
-        
-                    // loop again if has child nodes
-                    printNote(tempNode.getChildNodes());
-        
-                }
-        
-                System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
-        
+                Question newQuestion = new Question(id, questionContent, answer);
+                this.ruleRepository.addQuestion(newQuestion);
             }
-        
-            }
-        
-          }
-        
-        
-        if (document.hasChildNodes()) {
-            document.getChildNodes();
         }
+    }
+
+    private Answer getAnswers(Element tempElement) {
+        Answer answer = new Answer();
+        NodeList answersList = tempElement.getElementsByTagName("Selection");
+
+        for (int count = 0; count < answersList.getLength(); count++) {
+            Node answerNode = answersList.item(count);
+
+            if (answerNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element answerElement = (Element) answerNode;
+                Element valueNode = (Element) answerNode.getChildNodes().item(1);
+
+                boolean answerType = Boolean.valueOf(answerElement.getAttribute("value"));                
+                Value value;
+                
+                if (valueNode.getNodeName().equals("SingleValue")) {
+                    String param = valueNode.getAttribute("value");
+                    value = new SingleValue(param, answerType);    
+                } else {
+                    List<String> params = Arrays.asList(valueNode.getAttribute("value").split(","));
+                    value = new MultipleValue(params, answerType);
+                }
+                answer.addValue(value);
+            }
+        }
+        return answer;
     }
     
 }
