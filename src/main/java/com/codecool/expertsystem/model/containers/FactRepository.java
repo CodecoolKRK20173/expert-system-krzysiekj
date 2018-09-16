f69cbad7f1a34da17bf60628ee278b9909fa6fb5
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 18:25:47 +0200

    Implement inner Iterator class for FactRepository class

--- src/main/java/com/codecool/expertsystem/model/containers/FactRepository.java
+++ src/main/java/com/codecool/expertsystem/model/containers/FactRepository.java
@@ -14,21 +14,25 @@ public class FactRepository {
     }
 
     public void addFact(Fact fact) {
-
+        this.facts.add(fact);
     }
 
     private class FactIterator implements Iterator<Fact> {
+        int index;
 
         @Override
         public boolean hasNext() {
-    
+            return index < facts.size();
         }
     
         @Override
         public Fact next() {
-    
-        }
-    
+            if (this.hasNext()) {
+                return facts.get(index++);
+            } else {
+                return null;
+            }
+        }    
     }
 
     public Iterator<Fact> getIterator() {
