e1c0a2b90b71320bee625cc6b62754f89aa4c86d
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 16:42:52 +0200

    Implement SingleValue class

--- src/main/java/com/codecool/expertsystem/model/containers/SingleValue.java
+++ src/main/java/com/codecool/expertsystem/model/containers/SingleValue.java
@@ -1,9 +1,13 @@
 package com.codecool.expertsystem.model.containers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SingleValue extends Value {
 
     public SingleValue(String param, boolean selectionType) {
-
+        this.inputPattern = new ArrayList<>(Arrays.asList(param));
+        this.selectionType = selectionType;
     }
     
 }
