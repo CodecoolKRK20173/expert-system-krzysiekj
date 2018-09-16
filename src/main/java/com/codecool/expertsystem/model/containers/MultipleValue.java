92a73a9bf6dd8b498e43ed0116184d40227dfdd4
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 16:39:20 +0200

    Implement MultipleValue class

--- src/main/java/com/codecool/expertsystem/model/containers/MultipleValue.java
+++ src/main/java/com/codecool/expertsystem/model/containers/MultipleValue.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class MultipleValue extends Value {
 
     public MultipleValue(List<String> params, boolean selectionType) {
-
+        this.inputPattern = params;
+        this.selectionType = selectionType;
     }
 }
