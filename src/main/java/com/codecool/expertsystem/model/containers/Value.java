328dcd5b0b9a981e3d04906e3ee9c6e161a9d1d5
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-09-16 16:37:09 +0200

    Implement Value class

--- src/main/java/com/codecool/expertsystem/model/containers/Value.java
+++ src/main/java/com/codecool/expertsystem/model/containers/Value.java
@@ -3,13 +3,15 @@ package com.codecool.expertsystem.model.containers;
 import java.util.List;
 
 public abstract class Value {
+    List<String> inputPattern;
+    boolean selectionType;
 
     public List<String> getInputPattern() {
-
+        return this.inputPattern;
     }
 
     public boolean getSelectionType() {
-
+        return this.selectionType;
     }
     
 }
